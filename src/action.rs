ba831366474229afd62490cff4205a9d6f7cde97
Bijan Ghasemi Afshar <bijan.ghasemiafshar@gmail.com>
2023-06-11 11:59:24 +0100

    Removed non associate functions for Action

--- src/action.rs
+++ src/action.rs
@@ -41,7 +41,6 @@ pub struct Action<'a> {
     pub requires_arguments: bool,
     pub arguments: Vec<String>,
     pub error_logger: Option<&'a dyn Logger>,
-    pub execute: fn(Vec<String>) -> Result<(), &'static str>,
 }
 
 impl Action<'_> {
@@ -91,7 +90,7 @@ impl Action<'_> {
     }
 
     fn edit(self) -> Result<(), &'static str> {
-        let item_index: usize = get_item_index_arg(self.arguments)?;
+        let item_index: usize = self.get_item_index_arg()?;
 
         println!("Editing #{} ToDo item", item_index + 1);
         let mut todos: Vec<ToDo> = database::read_items()?;
@@ -127,7 +126,7 @@ impl Action<'_> {
     }
 
     fn update_todo_status(self, status: String) -> Result<(), &'static str> {
-        let item_index: usize = get_item_index_arg(self.arguments)?;
+        let item_index: usize = self.get_item_index_arg()?;
 
         println!("Editing #{} ToDo item", item_index + 1);
         let mut todos: Vec<ToDo> = database::read_items()?;
@@ -155,7 +154,7 @@ impl Action<'_> {
     }
 
     fn delete(self) -> Result<(), &'static str> {
-        let item_index: usize = get_item_index_arg(self.arguments)?;
+        let item_index: usize = self.get_item_index_arg()?;
 
         let mut todos: Vec<ToDo> = database::read_items()?;
 
@@ -171,6 +170,21 @@ impl Action<'_> {
 
         Ok(())
     }
+
+    fn get_item_index_arg(self) -> Result<usize, &'static str> {
+        match self.arguments.get(0) {
+            Some(arg) => match arg.parse::<usize>() {
+                Ok(index) => {
+                    if index == 0 {
+                        return Err("Given argument should be a positive number");
+                    }
+                    return Ok(index - 1);
+                }
+                Err(_) => return Err("Given argument should be a positive number"),
+            },
+            None => return Err("Could not get passed argument"),
+        };
+    }
 }
 
 impl Display for Action<'_> {
@@ -183,137 +197,6 @@ impl Display for Action<'_> {
     }
 }
 
-fn list(_: Vec<String>) -> Result<(), &'static str> {
-    println!("Printing all ToDo items");
-    let todos: Vec<ToDo> = database::read_items()?;
-
-    todos.iter().enumerate().for_each(|(index, todo)| {
-        println!(
-            "===============\n# {}\n{todo}\n===============\n",
-            index + 1
-        )
-    });
-
-    Ok(())
-}
-
-fn create(_: Vec<String>) -> Result<(), &'static str> {
-    println!("Creating a ToDo item");
-
-    print!("Title: ");
-    stdout().flush().unwrap();
-    let mut title = String::new();
-    io::stdin().read_line(&mut title).unwrap();
-
-    print!("Description: ");
-    stdout().flush().unwrap();
-    let mut description = String::new();
-    io::stdin().read_line(&mut description).unwrap();
-
-    let todo: ToDo = ToDo::new(title, description);
-
-    database::store_item(todo)?;
-
-    Ok(())
-}
-
-fn get_item_index_arg(args: Vec<String>) -> Result<usize, &'static str> {
-    match args.get(0) {
-        Some(arg) => match arg.parse::<usize>() {
-            Ok(index) => {
-                if index == 0 {
-                    return Err("Given argument should be a positive number");
-                }
-                return Ok(index - 1);
-            }
-            Err(_) => return Err("Given argument should be a positive number"),
-        },
-        None => return Err("Could not get passed argument"),
-    };
-}
-
-fn edit(args: Vec<String>) -> Result<(), &'static str> {
-    let item_index: usize = get_item_index_arg(args)?;
-
-    println!("Editing #{} ToDo item", item_index + 1);
-    let mut todos: Vec<ToDo> = database::read_items()?;
-
-    let mut edit_todo = match todos.get_mut(item_index) {
-        Some(todo) => todo,
-        None => return Err("Given ToDo Item index is wrong"),
-    };
-
-    println!("{edit_todo}");
-
-    print!("New Title: ");
-    stdout().flush().unwrap();
-    let mut new_title = String::new();
-    io::stdin().read_line(&mut new_title).unwrap();
-
-    print!("New Description: ");
-    stdout().flush().unwrap();
-    let mut new_description = String::new();
-    io::stdin().read_line(&mut new_description).unwrap();
-
-    if new_title.len() > 1 {
-        edit_todo.title = new_title;
-    }
-
-    if new_description.len() > 1 {
-        edit_todo.description = new_description;
-    }
-
-    database::store_existing_items(todos)?;
-
-    Ok(())
-}
-
-fn update_todo_status(args: Vec<String>, status: String) -> Result<(), &'static str> {
-    let item_index: usize = get_item_index_arg(args)?;
-
-    println!("Editing #{} ToDo item", item_index + 1);
-    let mut todos: Vec<ToDo> = database::read_items()?;
-
-    let mut edit_todo = match todos.get_mut(item_index) {
-        Some(todo) => todo,
-        None => return Err("Given ToDo Item index is wrong"),
-    };
-
-    edit_todo.done = status;
-
-    database::store_existing_items(todos)?;
-
-    Ok(())
-}
-
-fn done(args: Vec<String>) -> Result<(), &'static str> {
-    update_todo_status(args, "✅".into())?;
-    Ok(())
-}
-
-fn undone(args: Vec<String>) -> Result<(), &'static str> {
-    update_todo_status(args, "❌".into())?;
-    Ok(())
-}
-
-fn delete(args: Vec<String>) -> Result<(), &'static str> {
-    let item_index: usize = get_item_index_arg(args)?;
-
-    let mut todos: Vec<ToDo> = database::read_items()?;
-
-    if item_index >= todos.len() || item_index <= 0 {
-        return Err("Given item index is wrong");
-    }
-
-    println!("Deleting #{} ToDo item", item_index + 1);
-
-    todos.remove(item_index);
-
-    database::store_existing_items(todos)?;
-
-    Ok(())
-}
-
 pub const ACTIONS: [Action; 6] = [
     Action {
         action_type: ActionType::Create,
@@ -321,7 +204,6 @@ pub const ACTIONS: [Action; 6] = [
         requires_arguments: false,
         arguments: vec![],
         error_logger: None,
-        execute: create,
     },
     Action {
         action_type: ActionType::List,
@@ -329,7 +211,6 @@ pub const ACTIONS: [Action; 6] = [
         requires_arguments: false,
         arguments: vec![],
         error_logger: None,
-        execute: list,
     },
     Action {
         action_type: ActionType::Edit,
@@ -337,7 +218,6 @@ pub const ACTIONS: [Action; 6] = [
         requires_arguments: true,
         arguments: vec![],
         error_logger: None,
-        execute: edit,
     },
     Action {
         action_type: ActionType::Done,
@@ -345,7 +225,6 @@ pub const ACTIONS: [Action; 6] = [
         requires_arguments: true,
         arguments: vec![],
         error_logger: None,
-        execute: done,
     },
     Action {
         action_type: ActionType::Undone,
@@ -353,7 +232,6 @@ pub const ACTIONS: [Action; 6] = [
         requires_arguments: true,
         arguments: vec![],
         error_logger: None,
-        execute: undone,
     },
     Action {
         action_type: ActionType::Delete,
@@ -361,6 +239,5 @@ pub const ACTIONS: [Action; 6] = [
         requires_arguments: true,
         arguments: vec![],
         error_logger: None,
-        execute: delete,
     },
 ];
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
             process::exit(1);
         });
 
-    (valid_action.execute)(valid_action.arguments).unwrap_or_else(|err| {
+    valid_action.execute_action().unwrap_or_else(|err| {
         eprintln!("{err}");
         process::exit(1);
     });

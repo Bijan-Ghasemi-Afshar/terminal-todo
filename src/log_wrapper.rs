5a22efef6b002b320b3d3b98d7a497d862b4ee86
Bijan Ghasemi Afshar <bijan.ghasemiafshar@gmail.com>
2023-06-16 00:11:19 +0100

    Resolved naming warnings

--- src/log_wrapper.rs
+++ src/log_wrapper.rs
@@ -7,21 +7,21 @@ pub trait Logger {
     fn log_std<'a>(&mut self, msg: &'a str) -> Result<(), Box<dyn Error>>;
 }
 
-pub struct LogWrapper<ERR_W, STD_W>
+pub struct LogWrapper<ERRW, STDW>
 where
-    ERR_W: Write,
-    STD_W: Write,
+    ERRW: Write,
+    STDW: Write,
 {
-    err_writer: ERR_W,
-    std_writer: STD_W,
+    err_writer: ERRW,
+    std_writer: STDW,
 }
 
-impl<ERR_W, STD_W> LogWrapper<ERR_W, STD_W>
+impl<ERRW, STDW> LogWrapper<ERRW, STDW>
 where
-    ERR_W: Write,
-    STD_W: Write,
+    ERRW: Write,
+    STDW: Write,
 {
-    pub fn new(err_writer: ERR_W, std_writer: STD_W) -> Self {
+    pub fn new(err_writer: ERRW, std_writer: STDW) -> Self {
         LogWrapper {
             err_writer,
             std_writer,
@@ -29,10 +29,10 @@ where
     }
 }
 
-impl<ERR_W, STD_W> Logger for LogWrapper<ERR_W, STD_W>
+impl<ERRW, STDW> Logger for LogWrapper<ERRW, STDW>
 where
-    ERR_W: Write,
-    STD_W: Write,
+    ERRW: Write,
+    STDW: Write,
 {
     fn log_errln<'a>(&mut self, msg: &'a str) -> Result<(), Box<dyn Error>> {
         writeln!(self.err_writer, "{}", &msg)?;

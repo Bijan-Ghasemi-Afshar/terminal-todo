ad6f724b7e7bf60458e3f3db80fa1f0fe648135e
Bijan Ghasemi Afshar <bijan.ghasemiafshar@gmail.com>
2023-06-15 23:22:30 +0100

    Added inline std err and std out methods to Logger trait

--- src/log_wrapper.rs
+++ src/log_wrapper.rs
@@ -1,6 +1,8 @@
 use std::{error::Error, io::Write};
 
 pub trait Logger {
+    fn log_errln<'a>(&mut self, msg: &'a str) -> Result<(), Box<dyn Error>>;
+    fn log_stdln<'a>(&mut self, msg: &'a str) -> Result<(), Box<dyn Error>>;
     fn log_err<'a>(&mut self, msg: &'a str) -> Result<(), Box<dyn Error>>;
     fn log_std<'a>(&mut self, msg: &'a str) -> Result<(), Box<dyn Error>>;
 }
@@ -32,12 +34,20 @@ where
     ERR_W: Write,
     STD_W: Write,
 {
-    fn log_err<'a>(&mut self, msg: &'a str) -> Result<(), Box<dyn Error>> {
+    fn log_errln<'a>(&mut self, msg: &'a str) -> Result<(), Box<dyn Error>> {
         writeln!(self.err_writer, "{}", &msg)?;
         Ok(())
     }
-    fn log_std<'a>(&mut self, msg: &'a str) -> Result<(), Box<dyn Error>> {
+    fn log_stdln<'a>(&mut self, msg: &'a str) -> Result<(), Box<dyn Error>> {
         writeln!(self.std_writer, "{}", &msg)?;
+        Ok(())
+    }
+    fn log_err<'a>(&mut self, msg: &'a str) -> Result<(), Box<dyn Error>> {
+        write!(self.err_writer, "{}", &msg)?;
+        Ok(())
+    }
+    fn log_std<'a>(&mut self, msg: &'a str) -> Result<(), Box<dyn Error>> {
+        write!(self.std_writer, "{}", &msg)?;
         Ok(())
     }
 }
